17f50d8e19e3694f3611ac459f248226552b99fa
Igor Rodionov <goruha@gmail.com>
2025-11-16 02:48:17 +0100

    Simplify ECS task service

--- terraform/components/ecs-task/service.tf
+++ terraform/components/ecs-task/service.tf
@@ -1,43 +1,18 @@
-variable "ecs_cluster_arn" {
-  type        = string
-  description = "The ARN of the ECS cluster"
+locals {
+  hostname = replace(var.base_domains.public, "*", var.github_repo_name)
 }
 
-variable "subnet_ids" {
-  type        = list(string)
-  description = "The IDs of the subnets"
-}
+module "service_label" {
+  source     = "cloudposse/label/null"
+  version    = "0.25.0"
+  attributes = ["service"]
 
-variable "security_group_ids" {
-  type        = list(string)
-  description = "The IDs of the security groups"
-}
-
-variable "assign_public_ip" {
-  type        = bool
-  description = "Whether to assign a public IP address to the service"
-  default     = false
-}
-
-variable "vpc_id" {
-  type        = string
-  description = "The ID of the VPC"
-}
-
-variable "ecs_load_balancers" {
-  type = list(object({
-    container_name   = string
-    container_port   = number
-    elb_name         = optional(string)
-    target_group_arn = string
-  }))
-  description = "A list of load balancer config objects for the ECS service; see [ecs_service#load_balancer](https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service#load_balancer) docs"
-  default     = []
+  context = module.this.context
 }
 
 resource "aws_security_group" "ecs_service" {
   count       = module.this.enabled ? 1 : 0
-  vpc_id      = var.vpc_id
+  vpc_id      = var.vpc.vpc_id
   name        = module.service_label.id
   description = "Allow ALL egress from ECS service"
   tags        = module.service_label.tags
@@ -58,154 +33,127 @@ resource "aws_security_group_rule" "allow_all_egress" {
   security_group_id = one(aws_security_group.ecs_service[*]["id"])
 }
 
+resource "aws_security_group_rule" "allow_alb_ingress" {
+  count                    = module.this.enabled ? 1 : 0
+  description              = "Allow all outbound traffic to any IPv4 address"
+  type                     = "ingress"
+  from_port                = 0
+  to_port                  = 0
+  protocol                 = "-1"
+  source_security_group_id = var.lb.security_group_id
+  security_group_id        = one(aws_security_group.ecs_service[*].id)
+}
+
+module "alb_ingress" {
+  source  = "cloudposse/alb-ingress/aws"
+  version = "0.31.0"
+
+  count = module.this.enabled ? 1 : 0
+
+  vpc_id                        = var.vpc.vpc_id
+  unauthenticated_listener_arns = [var.lb.https_listener_arn]
+  unauthenticated_hosts = [local.hostname]
+  unauthenticated_paths = []
+  # When set to catch-all, make priority super high to make sure last to match
+  unauthenticated_priority     = 10
+  default_target_group_enabled = true
+
+  health_check_matcher             = "200-404"
+  health_check_path                = "/healthz"
+  health_check_port                = "traffic-port"
+  health_check_protocol            = "HTTP"
+  health_check_healthy_threshold   = 2
+  health_check_unhealthy_threshold = 2
+  health_check_interval            = 5
+  health_check_timeout             = 2
+  protocol                         = "HTTP"
+  port                             = 80
+
+  stickiness_enabled         = false
+  stickiness_type            = "lb_cookie"
+  stickiness_cookie_duration = 86400
+
+  context = module.service_label.context
+}
+
 resource "aws_ecs_service" "default" {
-  count                              = module.this.enabled ? 1 : 0
-  cluster                            = var.ecs_cluster_arn
-  name                               = module.this.id
-  task_definition                    = aws_ecs_task_definition.default[0].arn
-  desired_count                      = var.task.desired_count
-  deployment_maximum_percent         = var.task.deployment_maximum_percent
-  deployment_minimum_healthy_percent = var.task.deployment_minimum_healthy_percent
-  # availability_zone_rebalancing      = var.task.availability_zone_rebalancing
-  # health_check_grace_period_seconds  = var.task.health_check_grace_period_seconds
-  launch_type                        = length(var.task.capacity_provider_strategies) > 0 ? null : var.task.launch_type
-  # platform_version                   = var.task.launch_type == "FARGATE" ? var.task.platform_version : null
-  scheduling_strategy                = var.task.launch_type == "FARGATE" ? "REPLICA" : var.task.scheduling_strategy
-  # enable_ecs_managed_tags            = var.task.enable_ecs_managed_tags
-  # iam_role                           = var.task.enable_ecs_service_role ? coalesce(var.task.service_role_arn, one(aws_iam_role.ecs_service[*]["arn"])) : null
-  wait_for_steady_state              = var.task.wait_for_steady_state
-  # force_new_deployment               = var.task.force_new_deployment
-  # enable_execute_command             = var.task.enable_execute_command
+  count   = module.this.enabled ? 1 : 0
+  cluster = var.ecs.cluster_arn
+  name    = module.this.id
 
-#   dynamic "capacity_provider_strategy" {
-#     for_each = var.capacity_provider_strategies
-#     content {
-#       capacity_provider = capacity_provider_strategy.value.capacity_provider
-#       weight            = capacity_provider_strategy.value.weight
-#       base              = lookup(capacity_provider_strategy.value, "base", null)
-#     }
-#   }
+  task_definition = one(aws_ecs_task_definition.default[*].arn)
 
-#   dynamic "service_registries" {
-#     for_each = var.service_registries
-#     content {
-#       registry_arn   = service_registries.value.registry_arn
-#       port           = lookup(service_registries.value, "port", null)
-#       container_name = lookup(service_registries.value, "container_name", null)
-#       container_port = lookup(service_registries.value, "container_port", null)
-#     }
-#   }
+  scheduling_strategy = "REPLICA"
 
-#   dynamic "service_connect_configuration" {
-#     for_each = var.service_connect_configurations
-#     content {
-#       enabled   = service_connect_configuration.value.enabled
-#       namespace = service_connect_configuration.value.namespace
-#       dynamic "log_configuration" {
-#         for_each = try(service_connect_configuration.value.log_configuration, null) == null ? [] : [service_connect_configuration.value.log_configuration]
-#         content {
-#           log_driver = log_configuration.value.log_driver
-#           options    = log_configuration.value.options
-#           dynamic "secret_option" {
-#             for_each = length(log_configuration.value.secret_option) == 0 ? [] : [log_configuration.value.secret_option]
-#             content {
-#               name       = secret_option.value.name
-#               value_from = secret_option.value.value_from
-#             }
-#           }
-#         }
-#       }
-#       dynamic "service" {
-#         for_each = length(service_connect_configuration.value.service) == 0 ? [] : service_connect_configuration.value.service
-#         content {
-#           discovery_name        = service.value.discovery_name
-#           ingress_port_override = service.value.ingress_port_override
-#           port_name             = service.value.port_name
-#           dynamic "client_alias" {
-#             for_each = service.value.client_alias
-#             content {
-#               dns_name = client_alias.value.dns_name
-#               port     = client_alias.value.port
-#             }
-#           }
-#           dynamic "timeout" {
-#             for_each = length(service.value.timeout) == 0 ? [] : service.value.timeout
-#             content {
-#               idle_timeout_seconds        = timeout.value.idle_timeout_seconds
-#               per_request_timeout_seconds = timeout.value.per_request_timeout_seconds
-#             }
-#           }
-#           dynamic "tls" {
-#             for_each = length(service.value.tls) == 0 ? [] : service.value.tls
-#             content {
-#               kms_key  = tls.value.kms_key
-#               role_arn = tls.value.role_arn != null ? tls.value.role_arn : one(aws_iam_role.ecs_service_connect_tls[*].arn)
-#               issuer_cert_authority {
-#                 aws_pca_authority_arn = tls.value.issuer_cert_authority.aws_pca_authority_arn
-#               }
-#             }
-#           }
-#         }
-#       }
-#     }
-#   }
+  deployment_controller {
+    type = "ECS"
+  }
+  deployment_configuration {
+    strategy = "ROLLING"
+    # bake_time_in_minutes = 1
+    # Read more about lifecycle hooks here: https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service#lifecycle_hook
+    # lifecycle_hook {
+    # }
+  }
+  deployment_circuit_breaker {
+    enable   = true
+    rollback = true
+  }
+  sigint_rollback       = true
+  wait_for_steady_state = true
 
-#   dynamic "ordered_placement_strategy" {
-#     for_each = var.ordered_placement_strategy
-#     content {
-#       type  = ordered_placement_strategy.value.type
-#       field = lookup(ordered_placement_strategy.value, "field", null)
-#     }
-#   }
+  deployment_maximum_percent         = var.service.deployment_maximum_percent
+  deployment_minimum_healthy_percent = var.service.deployment_minimum_healthy_percent
 
-#   dynamic "placement_constraints" {
-#     for_each = var.service_placement_constraints
-#     content {
-#       type       = placement_constraints.value.type
-#       expression = lookup(placement_constraints.value, "expression", null)
-#     }
-#   }
+  availability_zone_rebalancing = "ENABLED"
 
-#   dynamic "load_balancer" {
-#     for_each = var.ecs_load_balancers
-#     content {
-#       container_name   = load_balancer.value.container_name
-#       container_port   = load_balancer.value.container_port
-#       elb_name         = lookup(load_balancer.value, "elb_name", null)
-#       target_group_arn = lookup(load_balancer.value, "target_group_arn", null)
-#     }
-#   }
-
-
-#   propagate_tags = var.propagate_tags
-#   tags           = var.use_old_arn ? null : module.this.tags
-
-#   deployment_controller {
-#     type = var.deployment_controller_type
-#   }
-
-#   # https://www.terraform.io/docs/providers/aws/r/ecs_service.html#network_configuration
-  dynamic "network_configuration" {
-    for_each = var.task.network_mode == "awsvpc" ? ["true"] : []
-    content {
-      security_groups  = compact(concat(var.security_group_ids, aws_security_group.ecs_service[*]["id"]))
-      subnets          = var.subnet_ids
-      assign_public_ip = var.task.assign_public_ip
-    }
+  capacity_provider_strategy {
+    capacity_provider = "FARGATE"
+    weight            = 1
+    base              = 1
   }
 
-#   dynamic "deployment_circuit_breaker" {
-#     for_each = var.deployment_controller_type == "ECS" ? ["true"] : []
-#     content {
-#       enable   = var.circuit_breaker_deployment_enabled
-#       rollback = var.circuit_breaker_rollback_enabled
-#     }
-#   }
+  # placement_constraints {
+  #   type = "distinctInstance"
+  #   expression = ""
+  # }
 
-#   triggers = local.redeployment_trigger
+  # ordered_placement_strategy {
+  #   type = "random"
+  #   # field = "attribute:ecs.availability-zone"
+  # }
+
+  enable_ecs_managed_tags = true
+  propagate_tags          = "TASK_DEFINITION"
+
+  enable_execute_command = var.service.enable_execute_command
+  force_new_deployment   = var.service.force_new_deployment
+
+  # https://www.terraform.io/docs/providers/aws/r/ecs_service.html#network_configuration
+  network_configuration {
+      security_groups  = compact(aws_security_group.ecs_service[*].id)
+      subnets          = var.vpc.private_subnet_ids
+      assign_public_ip = false
+  }
+
+  iam_role = one(aws_iam_role.ecs_service[*].arn)
+
+  load_balancer {
+    container_name   = "app"
+    container_port   = one(local.container_definitions["app"].portMappings[*].containerPort)
+    target_group_arn = one(module.alb_ingress[*].target_group_arn)
+  }
+  
+  # health_check_grace_period_seconds  = 10
+
+  tags = module.this.tags
 
   # Avoid race condition on destroy.
   # See https://registry.terraform.io/providers/hashicorp/aws/latest/docs/resources/ecs_service
-  # depends_on = [aws_iam_role.ecs_service, aws_iam_role_policy.ecs_service]
+  depends_on = [aws_iam_role.ecs_service, aws_iam_role_policy.ecs_service]
 
+  # Ignore changes to desired count as we use autoscaling instead
+  lifecycle {
+    ignore_changes = [desired_count]
+  }
 }
